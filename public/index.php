2f98b938000f12c21a7ad2940df2886925f1adc1
Dermot Bohan <bohan.dermot@gmail.com>
2017-03-19 04:53:54 +0000

    correcting grammar in index.php

--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@
 <div id="main">
   <div id="page">
     <h1>This is a test page</h1>
-  </div><!-- #page -->
+  </div><!-- /#page -->
 </div><!-- /#main -->
 <?php include('../includes/layouts/footer.php'); ?>
